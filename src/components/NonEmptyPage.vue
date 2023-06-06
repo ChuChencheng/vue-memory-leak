c0b32e9c23c10d315daa7d86ac160224ff4f47ec
ChuChencheng <azureternite@live.com>
2023-06-07 03:31:50 +0800

    first commit

---
+++ src/components/NonEmptyPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <select>
+      <option value="top">top</option>
+      <option value="center">center</option>
+      <option value="bottom">bottom</option>
+    </select>
+    <div v-for="item in 50000" :key="item">{{item}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NonEmptyPage",
+};
+</script>
